de373f222acd3a37dba11c0201860d9e8d9d41f8
jddesign17 <jddesign234@gmail.com>
2024-11-27 16:29:08 +0530

    changes

--- backend/index.js
+++ backend/index.js
@@ -1,24 +1,22 @@
-const express = require('express')
-const cors = require('cors')
-const app = express()
+const express = require("express");
+const cors = require("cors");
+const app = express();
 
-app.use(cors())
+app.use(cors());
 
 const fakeData = [
-    { name: "John Doe", age: 28, phone: "555-1234" },
-    { name: "Jane Smith", age: 34, phone: "555-5678" },
-    { name: "Michael Johnson", age: 41, phone: "555-8765" },
-    { name: "Emily Davis", age: 25, phone: "555-4321" },
-    { name: "David Wilson", age: 30, phone: "555-9876" }
-  ];
-  
+  { name: "John Doe", age: 28, phone: "555-1234" },
+  { name: "Jane Smith", age: 34, phone: "555-5678" },
+  { name: "Michael Johnson", age: 41, phone: "555-8765" },
+  { name: "Emily Davis", age: 25, phone: "555-4321" },
+  { name: "David Wilson", age: 30, phone: "555-9876" },
+];
 
-app.get('/get',(req,res)=>{
-    res.send(fakeData)
-})
+app.get("/get", (req, res) => {
+  res.send(fakeData);
+});
 
 
-app.listen(3000,()=>{
-    console.log("server started")
-})
-
+app.listen(3000, () => {
+  console.log("server started");
+});
